4e9213e62e3649394c4fed6929285a20821ba8ef
Igor Storchevoy <hellsgor@gmail.com>
2024-07-03 22:14:04 +0400

    add syncTermList function

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import './index.css';
 
 let terms = [];
 
+const form = document.getElementById('add-description');
+const descriptionList = document.getElementById('description-list');
+
+const reactRoot = createRoot(descriptionList);
+
+function syncTermList() {
+  reactRoot.render(<TermList terms={terms} />);
+}
+
 function addTerm(title, description) {
   terms.push({
     id: Date.now(),
@@ -12,25 +21,17 @@ function addTerm(title, description) {
   });
   terms.sort((term1, term2) => (term1.title < term2.title ? -1 : 1));
 
-  reactRoot.render(<TermList terms={terms} />);
+  syncTermList();
 }
 
-const form = document.getElementById('add-description');
-const descriptionList = document.getElementById('description-list');
-
-const reactRoot = createRoot(descriptionList);
-
-reactRoot.render(<TermList terms={terms} />);
+syncTermList();
 
 form.addEventListener('submit', (event) => {
-  // Отменяем поведение по умолчанию
   event.preventDefault();
 
-  // Получаем значения полей формы
   const title = form.elements['title'].value;
   const description = form.elements['description'].value;
 
-  // Сбрасываем форму
   form.reset();
 
   addTerm(title, description);
